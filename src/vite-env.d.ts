fd0d2de9a54c827b945c78173b48f6abb75e096f
Bruno Casotto <casottoalves@hotmail.com>
2023-12-20 16:47:16 -0300

    configure firebase hosting again

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -3,7 +3,7 @@ interface ImportMetaEnv {
   readonly VITE_APP_KEY: string
   readonly VITE_AUTH_DOMAIN: string
   readonly VITE_DATABASE_URL: string
-  readonly VITE_PRODCT_ID: string
+  readonly VITE_PROJECT_ID: string
   readonly VITE_STORAGE_BUCKET: string
   readonly VITE_MESSAGING_SENDER_ID: string
   readonly VITE_APP_ID: string
